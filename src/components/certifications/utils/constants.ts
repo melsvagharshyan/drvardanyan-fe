448c500411b9181387031b50e90b6c185d6f6200
Arsen <arsen.simonian@haqqex.com>
2025-08-04 16:56:17 +0400

    commit

--- src/components/certifications/utils/constants.ts
+++ src/components/certifications/utils/constants.ts
@@ -2,12 +2,12 @@ export const certifications = [
   {
     id: 1,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754310275/vahan/Blue_Minimalist_Certificate_Of_Achievement_zjdgtv.png',
+      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754311799/vahan/White_and_Grey_Bordered_Simple_Certificate_of_Appreciation_dvjpqd.png',
   },
   {
     id: 2,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754311799/vahan/White_and_Grey_Bordered_Simple_Certificate_of_Appreciation_dvjpqd.png',
+      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754310275/vahan/Blue_Minimalist_Certificate_Of_Achievement_zjdgtv.png',
   },
   {
     id: 3,
