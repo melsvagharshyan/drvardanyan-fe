04eabf7587ddc43b6973d7696cd25640154dcd7f
Arsen <arsen.simonian@haqqex.com>
2025-08-07 21:12:42 -0700

    diplom

--- src/components/certifications/utils/constants.ts
+++ src/components/certifications/utils/constants.ts
@@ -2,15 +2,25 @@ export const certifications = [
   {
     id: 1,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754311799/vahan/White_and_Grey_Bordered_Simple_Certificate_of_Appreciation_dvjpqd.png',
+      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754625985/vahan/AA_N%C2%BA_018793_page-0001_1_-Photoroom_ygnhg0.png',
   },
   {
     id: 2,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754310275/vahan/Blue_Minimalist_Certificate_Of_Achievement_zjdgtv.png',
+      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754626003/vahan/AD_N%C2%BA_096225_page-0001-Photoroom_phdacq.png',
   },
   {
     id: 3,
+    image:
+      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754311799/vahan/White_and_Grey_Bordered_Simple_Certificate_of_Appreciation_dvjpqd.png',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754310275/vahan/Blue_Minimalist_Certificate_Of_Achievement_zjdgtv.png',
+  },
+  {
+    id: 5,
     image:
       'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1754310926/vahan/Black_White_and_Gold_Elegant_Modern_Medical_Appreciation_Certificate_sl65mz.png',
   },
